f9b6bcd763a366191fb238f48037228a63677306
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-08-02 09:57:55 +0100

    Updated...

--- app/(root)/podcast/[podcastId]/page.tsx
+++ app/(root)/podcast/[podcastId]/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const PodcastDetails = ({ params }: { params : { podcastId: string }}) => {
+const PodcastDetails = ({ params  }: { params : { podcastId: string } }) => {
   return (
-    <p className='text-white-1'>PodcastDetails for {params.podcastId}</p>
+    <p className='text-white-1'>Podcast Details for {params.podcastId}</p>
   )
 }
 
