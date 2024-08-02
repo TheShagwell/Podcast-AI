b8390546a390a25bcad6dd75bca51d1ecf36272b
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-08-02 11:06:52 +0100

    Updated...

---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LeftSidebar = () => {
+  return (
+    <section>
+      <nav className="text-white-1">Left Sidebar</nav>
+    </section>
+  );
+};
+
+export default LeftSidebar;
