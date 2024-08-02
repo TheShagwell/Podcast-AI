c9f3a485146c368ecab51e08d081653fd5012e2f
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-08-02 10:34:46 +0100

    Updated...

---
+++ app/(root)/discover/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Discover = () => {
+  return (
+    <div className='text-violet-600'>Discover</div>
+  )
+}
+
+export default Discover
--- app/(root)/podcast/[podcastId]/page.tsx
+++ app/(root)/podcast/[podcastId]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const PodcastDetails = ({ params  }: { params : { podcastId: string } }) => {
+const PodcastDetails = ({ params }: { params : { podcastId: string } }) => {
   return (
     <p className='text-white-1'>Podcast Details for {params.podcastId}</p>
   )
