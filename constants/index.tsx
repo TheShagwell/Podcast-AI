c4d00a93ad3f96f7cadd6a3f1924e9ff2dd2f3ad
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-08-02 12:25:20 +0100

    Updated

---
+++ constants/index.tsx
@@ -0,0 +1,17 @@
+export const sidebarLinks = [
+    {
+        route: '/create-podcast',
+        label: "Create Podcast",
+        imgURL: '/icons/microphone.svg'
+    },
+    {
+        route: '/',
+        label: "Home",
+        imgURL: '/icons/home.svg'
+    },
+    {
+        route: '/discover',
+        label: "Discover",
+        imgURL: '/icons/discover.svg'
+    }
+]
