ca48d5dd696d8696dfa3bb3875efb11dfae9120e
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-08-02 11:38:41 +0100

    Updated

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,9 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const LeftSidebar = () => {
   return (
-    <section>
-      <nav className="text-white-1">Left Sidebar</nav>
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        <Link href="/">
+            <Image src="" alt="logo" width={23} height={27}/>
+        </Link>
+      </nav>
     </section>
   );
 };
