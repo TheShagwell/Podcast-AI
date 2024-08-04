4bdbb60c0afd415de50373c9262b13cb4f334605
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-08-04 15:03:09 +0100

    Updated the globals css for better UI visibility

--- components/shared/PodcastCard.tsx
+++ components/shared/PodcastCard.tsx
@@ -1,7 +1,18 @@
 import Image from 'next/image'
 import React from 'react'
 
-const PodcastCard = ({imgUrl, title, description, podcastId} : { imgUrl: string, title: string, description: string, podcastId: number}) => {
+const PodcastCard = ({
+  imgUrl, 
+  title, 
+  description, 
+  podcastId} 
+  : 
+  { 
+    imgUrl: string, 
+    title: string, 
+    description: string, 
+    podcastId: number
+  }) => {
   return (
     <div className='cursor-pointer'>
       <figure className='flex flex-col gap-2'>
