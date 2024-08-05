5626870bae16458fc5d34a3e376b19b40b6710a9
Erewharighe Ezra <kelvinerewharighe@gmail.com>
2024-08-05 15:11:45 +0100

    Updated the convex and clerk together

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://valid-silkworm-84.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+
+  export default authConfig;
